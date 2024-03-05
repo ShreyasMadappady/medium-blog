262a37e66ed07d640d39f322eb6d4833736abb32
Shreyas M S <shreyasmadappady20@gmail.com>
2024-03-05 09:46:51 +0530

    commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
@@ -8,15 +9,24 @@ const app = new Hono<{
   };
 }>();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
-
-app.post("/api/v1/signup", (c) => {
+app.post("/api/v1/signup", async (c) => {
   const prisma = new PrismaClient({
+    //@ts-ignore
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
-  return c.text("Hello Hono!");
+
+  const body = await c.req.json();
+
+  const user = await prisma.user.create({
+    data: {
+      email: c.body.email,
+      password: c.body.password,
+    },
+  });
+
+  const token = await sign({ id: user.id }, "Secret Key");
+
+  return c.json({ jwt: token });
 });
 app.post("/api/v1/signin", (c) => {
   return c.text("Hello Hono!");
